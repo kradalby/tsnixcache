eead35d7261f656f21a12e2a7654e29e870d26c2
Kristoffer Dalby <kristoffer@dalby.cc>
2026-09-10 13:49:17 +0000

    nixbase32: encode and decode Nix hashes

---
+++ nixbase32/bench_test.go
@@ -0,0 +1,101 @@
+// Copyright (c) 2026 Kristoffer Dalby
+// SPDX-License-Identifier: BSD-3-Clause
+
+package nixbase32
+
+import "testing"
+
+var (
+	sink      string
+	sinkBytes []byte
+)
+
+func BenchmarkEncodeToString_20(b *testing.B) {
+	input := make([]byte, 20)
+	for i := range input {
+		input[i] = byte(i * 13)
+	}
+
+	b.SetBytes(int64(len(input)))
+	b.ReportAllocs()
+
+	for b.Loop() {
+		sink = EncodeToString(input)
+	}
+}
+
+func BenchmarkEncodeToString_32(b *testing.B) {
+	input := make([]byte, 32)
+	for i := range input {
+		input[i] = byte(i * 7)
+	}
+
+	b.SetBytes(int64(len(input)))
+	b.ReportAllocs()
+
+	for b.Loop() {
+		sink = EncodeToString(input)
+	}
+}
+
+func BenchmarkEncodeToString_100(b *testing.B) {
+	input := make([]byte, 100)
+	for i := range input {
+		input[i] = byte(i)
+	}
+
+	b.SetBytes(int64(len(input)))
+	b.ReportAllocs()
+
+	for b.Loop() {
+		sink = EncodeToString(input)
+	}
+}
+
+func BenchmarkDecodeString_20(b *testing.B) {
+	input := make([]byte, 20)
+	for i := range input {
+		input[i] = byte(i * 13)
+	}
+
+	encoded := EncodeToString(input)
+
+	b.SetBytes(int64(len(input)))
+	b.ReportAllocs()
+
+	for b.Loop() {
+		sinkBytes, _ = DecodeString(encoded)
+	}
+}
+
+func BenchmarkDecodeString_32(b *testing.B) {
+	input := make([]byte, 32)
+	for i := range input {
+		input[i] = byte(i * 7)
+	}
+
+	encoded := EncodeToString(input)
+
+	b.SetBytes(int64(len(input)))
+	b.ReportAllocs()
+
+	for b.Loop() {
+		sinkBytes, _ = DecodeString(encoded)
+	}
+}
+
+func BenchmarkDecodeString_100(b *testing.B) {
+	input := make([]byte, 100)
+	for i := range input {
+		input[i] = byte(i)
+	}
+
+	encoded := EncodeToString(input)
+
+	b.SetBytes(int64(len(input)))
+	b.ReportAllocs()
+
+	for b.Loop() {
+		sinkBytes, _ = DecodeString(encoded)
+	}
+}
